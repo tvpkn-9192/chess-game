31f125e502428864abded278e924527830a82c0e
tvpkn <thirumlluripvankumar@gmail.com>
2024-06-16 13:38:03 +0530

    added flip board feature

--- src/app/modules/chess-board/chess-board.component.ts
+++ src/app/modules/chess-board/chess-board.component.ts
@@ -41,6 +41,12 @@ export class ChessBoardComponent {
       [FENChar.BlackKnight, FENChar.BlackBishop, FENChar.BlackRook, FENChar.BlackQueen];
   }
 
+  public flipMode: boolean = false;
+
+  public flipBoard(): void {
+    this.flipMode = !this.flipMode;
+  }
+
   public isSquareDark(x: number, y: number): boolean {
     return ChessBoard.isSquareDark(x, y);
   }
